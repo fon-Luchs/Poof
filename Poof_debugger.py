a9bdeb896d7bf46e64b53ca14d39320248e8d6a8
Vladislav <lvo123ua@gmail.com>
2018-08-13 16:41:34 +0300

    - add OpenProcess - add DebugActiveProcess - add  detach DebugActiveProcess

--- Poof_debugger.py
+++ Poof_debugger.py
@@ -9,7 +9,9 @@ kernel32 = windll.kernel32
 
 class debugger():
     def __init__(self):
-        pass
+        self.h_process       = None
+        self.pid             = None
+        self.debugger_active = False
 
     def load(self, path_to_exe):
         creation_flags = DBG_PROCESS
@@ -33,6 +35,27 @@ class debugger():
 
             print "[*] We have successfully launched the process!"
             print "[*] The Process ID I have is: %d" % process_information.dwProcessId
+            self.h_process = self.open_process(process_information.dwProcessId)
 
         else:
             print "[*] Error with error code %d" % kernel32.GetLastError()
+
+    def open_process(self, pid):
+        h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, pid, False)
+        return h_process
+
+    def attach(self, pid):
+        self.h_process = self.open_process(pid)
+        if kernel32.DebugActiveProcess(pid):
+            self.debugger_active = True
+            self.pid = int(pid)
+        else:
+            print "[*] Unable to attach to the process"
+
+    def detach(self):
+        if kernel32.DebugActiveProcess(self.pid):
+            print "[*] Finished debugging. Exiting..."
+            return True
+        else:
+            print "[*] There was an error"
+            return False
